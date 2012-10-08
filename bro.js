218c2030c66090c77adf283092ef15418cd37a7f
John Grishin <exah.nix@gmail.com>
2012-10-08 12:18:59 +0400

    Версия 0.3

--- bro.js
+++ bro.js
@@ -2,7 +2,7 @@
     Awesome Bro(wser) CSS Detector
     Copyright   Ivan Grishin
     License     MIT / http://bit.ly/mit-license
-    Version     0.2.1
+    Version     0.3
 
 **	 Inspired by HeadJS (http://headjs.com) & jQuery (http://jquery.com)
 **/
@@ -16,14 +16,15 @@
 	
 	var ismobile= ua.match(/(ipad)|(iphone)|(ipod)|(android)|(webos)/i)
     
-	ua = /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
-        /(opera)(?:.*version)?[ \/]([\w.]+)/.exec( ua ) ||
 	ua = /(iphone|ipod|ipad)/.exec(ua) ||
 		/(android)/.exec(ua) ||
+		/(webkit)[ \/]([\w.]+)/.exec(ua) ||
+        /(opera)(?:.*version)?[ \/]([\w.]+)/.exec(ua) ||
         /(msie) ([\w.]+)/.exec( ua ) ||
-        !/compatible/.test( ua ) && /(mozilla)(?:.*? rv:([\w.]+))?/.exec( ua ) ||
+        !/compatible/.test(ua) && /(mozilla)(?:.*? rv:([\w.]+))?/.exec(ua) ||
         ['','other', '0']; 
-        
+    
+    
     if (ua[1] == 'ipad') {
 		pushClass('webkit');
     } else if (ua[1] == 'ipod' || ua[1] == 'iphone') {
@@ -49,11 +50,10 @@
 		pushClass("ie" + parseFloat(ua[2]));
 		
 		// IE versions
-		for (var ver = 3; ver < 11; ver++) {
 		for (var ver = 5; ver < 15; ver++) {
 		   if (parseFloat(ua[2]) < ver) { pushClass("lt-ie" + ver); }
 		}
-	}
+	} 
 	
 	html.className += ' ' + cclass.join(' ');
 	html.className = html.className.replace('no-js', '');
